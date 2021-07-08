e0a0a7c7227ed58650ee40602e7fef979c3994e6
hakyong seo <hakyongseo@hakyongui-MacBookPro.local>
2021-07-08 23:52:19 +0900

    node,register

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const {User } = require("./models/User")
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
 //mongodb+srv://yongyong:<password>@cluster0.h5xhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
@@ -15,7 +24,20 @@ app.get('/', (req, res) => {
 })
 
 
+app.post('/register', (req, res) => {
 
+ 
+
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+      if(err) return res.json({success: false, err})
+      return res.status(200).json({
+        success:true
+      })
+    })
+
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
